7fa9b286af52b26b6db987b00a491472626772b4
Johan Frick <johan.frick@gmail.com>
2024-08-20 23:32:44 +0200

    Handle latency

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Box, Button, Center, Flex, Heading, Icon, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Flex, Heading, Icon, Spinner, Text } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { categories, names } from '@/app/const';
@@ -15,9 +15,12 @@ export default function Page() {
   const player = searchParams.get('player');
   const selectedName = player || names[0];
   const [stats, setStats] = useState<Statistic[]>([]);
+  const [loading, setLoading] = useState(true);
 
   function loadStats() {
-    fetchPlayerStatistics(selectedName).then(setStats);
+    fetchPlayerStatistics(selectedName)
+      .then(setStats)
+      .then(() => setLoading(false));
   }
 
   useEffect(loadStats, [selectedName]);
@@ -41,33 +44,62 @@ export default function Page() {
       </Center>
 
       <Flex justify='space-evenly' m='3' direction={'column'} gap='1'>
-        {categories.map((category) => {
-          const find = stats.find((stat) => stat.description === category.name);
-          const number = find ? find.count : 0;
-          const element = (
-            <Box key={category.name}>
-              {category.type !== prevCategory ? (
-                <Heading size='sm' mt='3' mb='1'>
-                  {category.type}
-                </Heading>
-              ) : (
-                ''
-              )}
-              <Flex align='center' gap='1'>
-                <Text flex='1'>{number}</Text>
-                <Button flex='1' colorScheme='red' onClick={async () => await stepStatistic(category, number - 1)} isDisabled={number === 0}>
-                  -
-                </Button>
-                <Button flex='1' colorScheme='green' onClick={async () => await stepStatistic(category, number + 1)}>
-                  +
-                </Button>
-                <Text flex='12'>{category.name}</Text>
-              </Flex>
-            </Box>
-          );
-          prevCategory = category.type;
-          return element;
-        })}
+        {loading ? (
+          <Spinner thickness='4px' speed='0.65s' emptyColor='gray.200' color='blue.500' size='xl' />
+        ) : (
+          categories.map((category) => {
+            const find = stats.find((stat) => stat.description === category.name);
+            const number = find ? find.count : 0;
+            const element = (
+              <Box key={category.name}>
+                {category.type !== prevCategory ? (
+                  <Heading size='sm' mt='3' mb='1'>
+                    {category.type}
+                  </Heading>
+                ) : (
+                  ''
+                )}
+                <Flex align='center' gap='1'>
+                  <Text flex='1'>{number}</Text>
+                  <Button
+                    flex='1'
+                    colorScheme='red'
+                    onClick={async () => {
+                      if (find && find.count > 0) {
+                        find.count--;
+                        setStats([...stats]);
+                      }
+                      await stepStatistic(category, number - 1);
+                    }}
+                    isDisabled={number === 0}>
+                    -
+                  </Button>
+                  <Button
+                    flex='1'
+                    colorScheme='green'
+                    onClick={async () => {
+                      if (find) {
+                        find.count++;
+                      } else {
+                        stats.push({
+                          description: category.name,
+                          player: selectedName,
+                          count: 1,
+                        });
+                      }
+                      setStats([...stats]);
+                      await stepStatistic(category, number + 1);
+                    }}>
+                    +
+                  </Button>
+                  <Text flex='12'>{category.name}</Text>
+                </Flex>
+              </Box>
+            );
+            prevCategory = category.type;
+            return element;
+          })
+        )}
       </Flex>
     </>
   );
--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,10 +1,10 @@
 'use client';
-import { Button, Flex, FormControl, FormLabel, Heading, Input, useToast } from '@chakra-ui/react';
+import { Button, Flex, FormControl, FormLabel, Heading, Input, Spinner, useToast } from '@chakra-ui/react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import React, { Suspense, useEffect, useState } from 'react';
-import { colors } from '@/app/const';
+import React, { useEffect, useState } from 'react';
 import { deletePlayer, fetchPlayersFromDb, savePlayer } from '@/app/lib/db';
 import { Player } from '@/app/types';
+import { colors } from '@/app/const';
 import { FaRegTrashCan } from 'react-icons/fa6';
 
 function getHeading(text: string, mt = 10) {
@@ -16,49 +16,19 @@ function getHeading(text: string, mt = 10) {
 }
 
 export default function Page() {
-  function PlayerButton({ player, index }: { player: Player; index: number }) {
-    const searchParams = useSearchParams();
-    const selectedPlayer = searchParams.get('player');
-    return (
-      <Button
-        variant={player.name === selectedPlayer ? 'outline' : 'solid'}
-        key={player.name}
-        m='2'
-        onClick={() => {
-          if (deleteMode) {
-            toast.promise(deletePlayer(player.name), {
-              success: () => {
-                loadPlayers();
-                return {
-                  title: 'Spelaren raderad!',
-                };
-              },
-              error: (err) => {
-                return {
-                  title: 'Spelaren raderades inte',
-                  description: err.message,
-                };
-              },
-              loading: { title: 'Raderas...' },
-            });
-          } else {
-            router.push(`/?player=${player.name}`);
-          }
-        }}
-        colorScheme={colors[index % colors.length]}
-        rightIcon={deleteMode ? <FaRegTrashCan /> : undefined}>
-        {player.name}
-      </Button>
-    );
-  }
+  const searchParams = useSearchParams();
+  const selectedPlayer = searchParams.get('player');
 
   const router = useRouter();
   const [players, setPlayers] = useState<Player[]>([]);
   const [deleteMode, setDeleteMode] = useState<Boolean>(false);
+  const [loading, setLoading] = useState(true);
   const toast = useToast();
 
   function loadPlayers() {
-    fetchPlayersFromDb().then(setPlayers);
+    fetchPlayersFromDb()
+      .then(setPlayers)
+      .then(() => setLoading(false));
   }
 
   useEffect(loadPlayers, []);
@@ -68,46 +38,78 @@ export default function Page() {
     <>
       <Flex direction='column' align='center'>
         {getHeading('Välj spelare', 3)}
-        <Flex wrap='wrap'>
-          {players.map((player, index) => (
-            <Suspense key={index}>
-              <PlayerButton player={player} index={index} />
-            </Suspense>
-          ))}
-        </Flex>
-        {getHeading('Skapa ny spelare')}
-        <form
-          action={() => {
-            toast.promise(savePlayer(newPlayer), {
-              success: () => {
-                setNewPlayer('');
-                loadPlayers();
-                return {
-                  title: 'Sparad!',
-                };
-              },
-              error: (err) => {
-                return {
-                  title: 'Spelaren sparades inte',
-                  description: err.message,
-                };
-              },
-              loading: { title: 'Sparas...' },
-            });
-          }}>
-          <FormControl isRequired>
-            <FormLabel>Namn</FormLabel>
-            <Input name='name' type='text' value={newPlayer} placeholder='spelarens namn' size='lg' onChange={handleChange} />
-          </FormControl>
-          <Button variant='outline' type='submit' width='full' mt={4}>
-            Spara
-          </Button>
-        </form>
+        {loading ? (
+          <Spinner thickness='4px' speed='0.65s' emptyColor='gray.200' color='blue.500' size='xl' />
+        ) : (
+          <>
+            <Flex wrap='wrap'>
+              {players.map((player, index) => (
+                <Button
+                  variant={player.name === selectedPlayer ? 'outline' : 'solid'}
+                  key={player.name}
+                  m='2'
+                  onClick={() => {
+                    if (deleteMode) {
+                      toast.promise(deletePlayer(player.name), {
+                        success: () => {
+                          loadPlayers();
+                          return {
+                            title: 'Spelaren raderad!',
+                          };
+                        },
+                        error: (err) => {
+                          return {
+                            title: 'Spelaren raderades inte',
+                            description: err.message,
+                          };
+                        },
+                        loading: { title: 'Raderas...' },
+                      });
+                    } else {
+                      router.push(`/?player=${player.name}`);
+                    }
+                  }}
+                  colorScheme={colors[index % colors.length]}
+                  rightIcon={deleteMode ? <FaRegTrashCan /> : undefined}>
+                  {player.name}
+                </Button>
+              ))}
+            </Flex>
+            {getHeading('Skapa ny spelare')}
+            <form
+              action={() => {
+                toast.promise(savePlayer(newPlayer), {
+                  success: () => {
+                    setNewPlayer('');
+                    loadPlayers();
+                    return {
+                      title: 'Sparad!',
+                    };
+                  },
+                  error: (err) => {
+                    return {
+                      title: 'Spelaren sparades inte',
+                      description: err.message,
+                    };
+                  },
+                  loading: { title: 'Sparas...' },
+                });
+              }}>
+              <FormControl isRequired>
+                <FormLabel>Namn</FormLabel>
+                <Input name='name' type='text' value={newPlayer} placeholder='spelarens namn' size='lg' onChange={handleChange} />
+              </FormControl>
+              <Button variant='outline' type='submit' width='full' mt={4}>
+                Spara
+              </Button>
+            </form>
 
-        {getHeading('Ta bort spelare')}
-        <Button colorScheme='red' onClick={() => setDeleteMode(!deleteMode)}>
-          {deleteMode ? 'Klar' : 'Välj spelare...'}
-        </Button>
+            {getHeading('Ta bort spelare')}
+            <Button colorScheme='red' onClick={() => setDeleteMode(!deleteMode)}>
+              {deleteMode ? 'Klar' : 'Välj spelare...'}
+            </Button>
+          </>
+        )}
       </Flex>
     </>
   );
